64ef16fa3a9767cb74c236fe9ca0ee4ac0d9551d
Carlo Emilio Montanari <carlidel@ymail.com>
2018-11-26 11:58:51 +0100

    parameters.py is not useful as concept

--- genetic_algorithm.py
+++ genetic_algorithm.py
@@ -11,6 +11,18 @@ from network_tools import *
 Implementation of a generic Genetic Algorithm optimizer
 for optimal laplacian mass tuning.
 """
+# GA parameters
+population_size = 100
+elite_rate = 0.10
+random_rate = 0.10
+mutation_rate = 0.5
+mutation_part = 0.3  # which percentage of genes mutate?
+max_iterations = 100
+precision = 0.001
+
+value_resolution = 10
+min_mass = 0.
+max_mass = 10.
 
 elite_size = int(population_size * elite_rate)
 random_size = int(population_size * random_rate)
